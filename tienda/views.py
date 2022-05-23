9de514bb344df6a530fbd9229b0fadb2d5505953
Joel Ortega <joelor2001@gmail.com>
2022-05-23 20:59:36 +0200

    V.3

--- tienda/views.py
+++ tienda/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a la Tienda")
+    return HttpResponse("<h1>Bienvenido a la Tienda</h1>")
+
+
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
